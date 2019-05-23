3f8d8864a6870c77ae0f3790fe6eb9f73dd04136
Calle <36276161+qalle-fivem@users.noreply.github.com>
2019-05-23 18:36:41 +0200

    Added config, camera and new features.

--- client/main.lua
+++ client/main.lua
@@ -5,6 +5,8 @@ RegisterCommand("rc", function()
 end)
 
 RCCar.Start = function()
+	if DoesEntityExist(RCCar.Entity) then return end
+
 	RCCar.Spawn()
 
 	RCCar.Tablet(true)
@@ -12,80 +14,151 @@ RCCar.Start = function()
 	while DoesEntityExist(RCCar.Entity) and DoesEntityExist(RCCar.Driver) do
 		Citizen.Wait(5)
 
-		local rcCarPos = GetEntityCoords(RCCar.Entity)
+		local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()),  GetEntityCoords(RCCar.Entity), true)
 
-		local distanceCheck = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), rcCarPos, true)
+		RCCar.DrawInstructions(distanceCheck)
+		RCCar.HandleKeys(distanceCheck)
 
-		TaskPause(RCCar.Driver, 1000)
-
-		if distanceCheck <= 100.0 then
+		if distanceCheck <= Config.LoseConnectionDistance then
 			if not NetworkHasControlOfEntity(RCCar.Driver) then
 				NetworkRequestControlOfEntity(RCCar.Driver)
 			elseif not NetworkHasControlOfEntity(RCCar.Entity) then
 				NetworkRequestControlOfEntity(RCCar.Entity)
 			end
-
-			RCCar.HandleKeys()
-
-			DrawScreenText("Pick up your ~g~RC~s~ to ~r~cancel~s~ the driving!")
 		else
 			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 6, 2500)
-
-			DrawScreenText("You're losing the ~r~contact~s~ with your RC!")
-		end
-
-		if distanceCheck <= 1.5 then
-			Draw3DText(rcCarPos, "[~g~E~s~] Pick", 0.4)
-
-			if IsControlJustPressed(0, 38) then
-				RCCar.Attach("pick")
-			end
-		else
-			Draw3DText(rcCarPos, "~b~RC", 0.4)
 		end
 	end
 end
 
-RCCar.HandleKeys = function()
-	if IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 9, 1)
-	end
-	
-	if IsControlJustReleased(0, 172) or IsControlJustReleased(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 6, 2500)
-	end
-
-	if IsControlPressed(0, 173) and not IsControlPressed(0, 172) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 22, 1)
+RCCar.HandleKeys = function(distanceCheck)
+	if IsControlJustReleased(0, 47) then
+		if IsCamRendering(RCCar.Camera) then
+			RCCar.ToggleCamera(false)
+		else
+			RCCar.ToggleCamera(true)
+		end
 	end
 
-	if IsControlPressed(0, 174) and IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 13, 1)
+	if distanceCheck <= 1.5 then
+		if IsControlJustPressed(0, 38) then
+			RCCar.Attach("pick")
+		end
 	end
 
-	if IsControlPressed(0, 175) and IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 14, 1)
+	if distanceCheck < Config.LoseConnectionDistance then
+		if IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 9, 1)
+		end
+		
+		if IsControlJustReleased(0, 172) or IsControlJustReleased(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 6, 2500)
+		end
+
+		if IsControlPressed(0, 173) and not IsControlPressed(0, 172) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 22, 1)
+		end
+
+		if IsControlPressed(0, 174) and IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 13, 1)
+		end
+
+		if IsControlPressed(0, 175) and IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 14, 1)
+		end
+
+		if IsControlPressed(0, 172) and IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 30, 100)
+		end
+
+		if IsControlPressed(0, 174) and IsControlPressed(0, 172) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 7, 1)
+		end
+
+		if IsControlPressed(0, 175) and IsControlPressed(0, 172) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 8, 1)
+		end
+
+		if IsControlPressed(0, 174) and not IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 4, 1)
+		end
+
+		if IsControlPressed(0, 175) and not IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
+			TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 5, 1)
+		end
+	end
+end
+
+RCCar.DrawInstructions = function(distanceCheck)
+	local steeringButtons = {
+		{
+			["label"] = "Right",
+			["button"] = "~INPUT_CELLPHONE_RIGHT~"
+		},
+		{
+			["label"] = "Forward",
+			["button"] = "~INPUT_CELLPHONE_UP~"
+		},
+		{
+			["label"] = "Reverse",
+			["button"] = "~INPUT_CELLPHONE_DOWN~"
+		},
+		{
+			["label"] = "Left",
+			["button"] = "~INPUT_CELLPHONE_LEFT~"
+		}
+	}
+
+	local pickupButton = {
+		["label"] = "Pick Up",
+		["button"] = "~INPUT_CONTEXT~"
+	}
+
+	local buttonsToDraw = {
+		{
+			["label"] = "Toggle Camera",
+			["button"] = "~INPUT_DETONATE~"
+		}
+	}
+
+	if distanceCheck <= Config.LoseConnectionDistance then
+		for buttonIndex = 1, #steeringButtons do
+			local steeringButton = steeringButtons[buttonIndex]
+
+			table.insert(buttonsToDraw, steeringButton)
+		end
+
+		if distanceCheck <= 1.5 then
+			table.insert(buttonsToDraw, pickupButton)
+		end
 	end
 
-	if IsControlPressed(0, 172) and IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 30, 100)
-	end
+    Citizen.CreateThread(function()
+        local instructionScaleform = RequestScaleformMovie("instructional_buttons")
 
-	if IsControlPressed(0, 174) and IsControlPressed(0, 172) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 7, 1)
-	end
+        while not HasScaleformMovieLoaded(instructionScaleform) do
+            Wait(0)
+        end
 
-	if IsControlPressed(0, 175) and IsControlPressed(0, 172) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 8, 1)
-	end
+        PushScaleformMovieFunction(instructionScaleform, "CLEAR_ALL")
+        PushScaleformMovieFunction(instructionScaleform, "TOGGLE_MOUSE_BUTTONS")
+        PushScaleformMovieFunctionParameterBool(0)
+        PopScaleformMovieFunctionVoid()
 
-	if IsControlPressed(0, 174) and not IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 4, 1)
-	end
+        for buttonIndex, buttonValues in ipairs(buttonsToDraw) do
+            PushScaleformMovieFunction(instructionScaleform, "SET_DATA_SLOT")
+            PushScaleformMovieFunctionParameterInt(buttonIndex - 1)
 
-	if IsControlPressed(0, 175) and not IsControlPressed(0, 172) and not IsControlPressed(0, 173) then
-		TaskVehicleTempAction(RCCar.Driver, RCCar.Entity, 5, 1)
-	end
+            PushScaleformMovieMethodParameterButtonName(buttonValues["button"])
+            PushScaleformMovieFunctionParameterString(buttonValues["label"])
+            PopScaleformMovieFunctionVoid()
+        end
+
+        PushScaleformMovieFunction(instructionScaleform, "DRAW_INSTRUCTIONAL_BUTTONS")
+        PushScaleformMovieFunctionParameterInt(-1)
+        PopScaleformMovieFunctionVoid()
+        DrawScaleformMovieFullscreen(instructionScaleform, 255, 255, 255, 255)
+    end)
 end
 
 RCCar.Spawn = function()
@@ -133,6 +206,10 @@ RCCar.Attach = function(param)
 
 		PlaceObjectOnGroundProperly(RCCar.Entity)
 	elseif param == "pick" then
+		if DoesCamExist(RCCar.Camera) then
+			RCCar.ToggleCamera(false)
+		end
+
 		RCCar.Tablet(false)
 
 		Citizen.Wait(100)
@@ -182,6 +259,46 @@ RCCar.Tablet = function(boolean)
 	end
 end
 
+RCCar.ToggleCamera = function(boolean)
+	if not Config.Camera then return end
+
+	if boolean then
+		if not DoesEntityExist(RCCar.Entity) then return end 
+		if DoesCamExist(RCCar.Camera) then DestroyCam(RCCar.Camera) end
+
+		RCCar.Camera = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
+
+		AttachCamToEntity(RCCar.Camera, RCCar.Entity, 0.0, 0.0, 0.4, true)
+
+		Citizen.CreateThread(function()
+			while DoesCamExist(RCCar.Camera) do
+				Citizen.Wait(5)
+
+				SetCamRot(RCCar.Camera, GetEntityRotation(RCCar.Entity))
+			end
+		end)
+
+		local easeTime = 500 * math.ceil(GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(RCCar.Entity), true) / 10)
+
+		RenderScriptCams(1, 1, easeTime, 1, 1)
+
+		Citizen.Wait(easeTime)
+
+		SetTimecycleModifier("scanline_cam_cheap")
+		SetTimecycleModifierStrength(2.0)
+	else
+		local easeTime = 500 * math.ceil(GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(RCCar.Entity), true) / 10)
+
+		RenderScriptCams(0, 1, easeTime, 1, 0)
+
+		Citizen.Wait(easeTime)
+
+		ClearTimecycleModifier()
+
+		DestroyCam(RCCar.Camera)
+	end
+end
+
 RCCar.LoadModels = function(models)
 	for modelIndex = 1, #models do
 		local model = models[modelIndex]
@@ -218,34 +335,4 @@ RCCar.UnloadModels = function()
 			RemoveAnimDict(model)   
 		end
 	end
-end
-
-DrawScreenText = function(text)
-    SetTextFont(0)
-    SetTextProportional(0)
-    SetTextScale(0.4, 0.4)
-    SetTextColour(255, 255, 255, 255)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(0.82 - 1.0/2, 0.604 - 1.0/2 + 0.005)
-end
-
-Draw3DText = function(coords, text, size)
-	local onScreen, _x, _y=World3dToScreen2d(coords["x"], coords["y"], coords["z"])
-	
-	SetTextScale(0.35, 0.35)
-	SetTextFont(4)
-	SetTextProportional(1)
-	SetTextColour(255, 255, 255, 215)
-	SetTextEntry("STRING")
-	SetTextCentre(1)
-	AddTextComponentString(text)
-	DrawText(_x, _y)
-
-	local factor = (string.len(text)) / 370
-	DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 41, 11, 41, 68)
 end
